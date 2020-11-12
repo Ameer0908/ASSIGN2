febf6521e290b68db3be530fd219a281b7b432f5
Syed Ameer Hamza <69245183+Ameer0908@users.noreply.github.com>
2020-11-13 00:37:51 +0500

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Changes made by 2017453!
+          RANA DANGEROUS!
         </p>
         <a
           className="App-link"
